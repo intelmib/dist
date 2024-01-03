300ea17535b8daddbe724d8a707c6657ceead7ef
Renan Kemp <intelmib@gmail.com>
2024-01-03 16:17:01 +0000

    20230103

---
+++ js/chunk-0d2f240e.js
@@ -0,0 +1,1 @@
+import{_ as t,r as o,o as n,c as r,w as s,e as a}from"./bd35568a.js";const p={};function _(u,l){const e=o("AppLocal"),c=o("Layout");return n(),r(c,null,{default:s(()=>[a(e,{resource:"/admin/groups/products"})]),_:1})}const f=t(p,[["render",_]]);export{f as default};
---
+++ js/chunk-e67820c8.js
@@ -0,0 +1,1 @@
+import{_ as c,r as e,o as n,c as a,w as r,e as s}from"./bd35568a.js";const _={};function l(p,u){const t=e("AppLocal"),o=e("Layout");return n(),a(o,null,{default:r(()=>[s(t,{resource:"/admin/nfe",risk:!0,"hide-create-table":!0,title:"NF-e"})]),_:1})}const i=c(_,[["render",l]]);export{i as default};
